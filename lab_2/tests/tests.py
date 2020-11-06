77fcd8cbf8400214fa3cd8633a3538f4c6a4bd1d
viktoriiacherevan <72121691+viktoriiacherevan@users.noreply.github.com>
2020-11-06 14:03:55 +0200

    lab_2

---
+++ lab_2/tests/tests.py
@@ -0,0 +1,32 @@
+import unittest
+from app import main, greetings
+
+
+class TestClass(unittest.TestCase):
+    def setUp(self):
+        # Дана функція налаштовує початкові агрументи визначені лише для класу
+        self.date_url = 'http://date.jsontest.com/'
+        self.ip_url = 'http://ip.jsontest.com/'
+        self.test_result = greetings(12)
+
+    def test_my_method(self):
+        self.assertTrue(self.test_result == "Добрий день!")
+
+
+
+    def test_date_work_successfully(self):
+        # Перевіряєм чи функція відправювала до кінця і повернулі True
+        self.assertTrue(main(self.date_url))
+
+    def test_empty_url(self):
+        # Перевіряєм чи у функцію не було передано жодної URL
+        self.assertFalse(main())
+
+    def test_no_date_in_response(self):
+        # Перевіряємо що у відповіді відсутнє поле дата (тобто передана направильна URL)
+        with self.assertRaises(Exception):
+            main(self.ip_url)
+
+    def test_home_work(self):
+        # Ваш захист
+        self.assertTrue(True)
