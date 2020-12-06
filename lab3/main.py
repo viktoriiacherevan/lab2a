244ff5b92b0012e8b8ba2b7dc29d40bba47ca346
viktoriiacherevan <72121691+viktoriiacherevan@users.noreply.github.com>
2020-12-06 13:31:51 +0200

    lab3:serverlog

--- lab3/main.py
+++ lab3/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import requests
+import json
+import logging
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logging.basicConfig(
+    filename="server.log",
+    filemode='a',
+    level=logging.INFO,
+    format='{levelname} {asctime} {name} : {message}',
+    style='{'
+)
+log = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    try:
+        r = requests.get(get_url())
+        data = json.loads(r.content)
+        logging.info("Сервер доступний. Час на сервері: %s", data['date'])
+        logging.info("Запитувана сторінка: : %s", data['current_page'])
+        logging.info("Відповідь сервера місти наступні поля:")
+        for key in data.keys():
+            logging.info("Ключ: %s, Значення: %s", key, data[key])
+    except Exception as e:
+        logging.error("Something happened wrong:" + str(e))
 
 
-# Press the green button in the gutter to run the script.
+def get_url():
+    return "http://localhost:8000/health"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        main()
+        time.sleep(60)
--- lab3/main1/views.py
+++ lab3/main1/views.py
@@ -9,5 +9,20 @@ def main(request):
 
 
 def health(request):
-    response = {'date': 'test1', 'current_page': "test2", 'server_info': "test3", 'client_info': "test4"}
+    response = {'date': datetime.now().strftime("%d-%m-%Y %H:%M"),
+                'current_page': request.build_absolute_uri(),
+                'server_info':
+                    {
+                        'OS Name': os.name,
+                        'User name': os.getlogin(),
+                     },
+                'client_info':
+                    {
+                        'User agent': request.headers['user-agent'],
+                        'Remote address': request.META['REMOTE_ADDR'],
+                        'Host Header': request.META['HTTP_HOST'],
+                        'Remote host': request.META['REMOTE_HOST'],
+
+                    }
+                }
     return JsonResponse(response)
