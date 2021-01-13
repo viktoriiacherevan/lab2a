3bca5c87d1189d7c2b6e061da3d939d98e8735db
viktoriiacherevan <72121691+viktoriiacherevan@users.noreply.github.com>
2021-01-13 18:33:22 +0200

    Update settings.py

--- lab3/my_site/settings.py
+++ lab3/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1ips&x#ke9mtzosqj6ry@p+z5avwje8xve*9ptv3w4r(+^ijbn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
